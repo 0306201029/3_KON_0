4bdb066968f60034c9611082a4373af4a2cd3942
quocthai027 <113010699+quocthai027@users.noreply.github.com>
2022-09-13 16:05:17 +0700

    upload

--- us02/thai.dart
+++ us02/thai.dart
@@ -1,3 +1,15 @@
- void main(List<String> args) {
-  print("ads");
+ void main() {
+   // declare an integer
+   int num1 = 10;             
+     
+   // declare a double value
+   double num2 = 10.50;  
+
+   // print the values
+   print(num1);
+   print(num2);
+   print(num.parse('12')); 
+   print(num.parse('10.91'));
+   var a = -2; 
+   print(a.abs());
 }
